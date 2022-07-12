d80a24bb184e02d37e0f6794465814a3def07c9c
DyanaFurriel <dyanafurriel.dsf@gmail.com>
2022-07-12 10:59:15 -0300

    Exercicio6

---
+++ scritp.js
@@ -0,0 +1,61 @@
+// Crie um objeto que receba ao menos três propriedades sobre você.
+// Adicione uma nova propriedade sem alterar seu objeto inicial.
+// Remova uma propriedade desse objeto.
+//Mostre no console todas as propriedades desse objeto.
+
+let englisProf ={
+    nome: 'Dyana',
+    idade: 26,
+    cidade: "Rio de Janeiro"
+}
+
+englisProf.signo = "Câncer";
+delete englishProf.idade;
+console.log(englisProf)
+
+
+
+// Crie um array  chamado "cadastro" contendo ao menos 5 objetos. 
+//Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
+//Na propriedade amigos, adicione ao menos 4 itens.
+// Mostre no console o nome de um amigo de cada lista.
+
+let cadastro =[{
+    nome:"Falula",
+    idade:35,
+    telefone:555667868,
+    amigo: ['Monica', 'Rachel', 'Joy', 'Chanler', 'Phoebe']
+},
+
+{
+    nome:"Camila",
+    idade:50,
+    telefone:755667868,
+    amigo: ['Bianca', 'Rian', 'Jonas', 'Charles', 'Patricia']
+},
+
+{
+    nome:"Mirian",
+    idade:29,
+    telefone:555667868,
+    amigo: ['Thiago', 'Rafael', 'Ester', 'Louise', 'Rebeca']
+},
+
+{
+    nome:"Mylena",
+    idade:4,
+    telefone:555667868,
+    amigo: ['Rosangela', 'Paulo', 'Bruno', 'Mirella', 'Cris']
+},
+
+{
+    nome:"Lucas",
+    idade:20,
+    telefone:555667868,
+    amigo: ['Luiz', 'Matheus', 'André', 'amora', 'Moisés']
+}]
+
+
+
+
+
